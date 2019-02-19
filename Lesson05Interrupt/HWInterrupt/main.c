05ab4ed06d13cfe35f01b5dcd0bf207616d29300
tiktak1028 <richardarellaga@ucu.edu.ph>
2019-02-19 16:27:01 +0800

    Update main.c

--- Lesson05Interrupt/HWInterrupt/main.c
+++ Lesson05Interrupt/HWInterrupt/main.c
@@ -15,7 +15,28 @@
 
 #define BUTTON                  PD2
 
-ISR(INT0_vect) {         /* Run every time there is a change on button */
+
+void initInterrupt0 (void);
+
+int main(void) {
+  
+  LED_DDR |= (1<<LED0) | (1<<LED1);                                    
+  BUTTON_PORT |= (1 << BUTTON); //enable button pull-ups                     
+  initInterrupt0(); 
+
+ 
+  while (1) {
+
+   
+    LED_PORT ^= (1 << LED0);
+	 _delay_ms(200);
+
+  }                                                 
+  return 0;                           
+}
+
+ISR(INT0_vect) {      
+ /* Run every time there is a change on button */  
   if ((BUTTON_PIN & (1<< BUTTON))== 0) {
     LED_PORT |= (1 << LED1);
   }
@@ -25,23 +46,7 @@ ISR(INT0_vect) {         /* Run every time there is a change on button */
 }
 
 void initInterrupt0(void) {
-  EIMSK |= (1 << INT0);                                 /* enable INT0 */
-  EICRA |= (1 << ISC00);                /* trigger when button changes */
-  sei();                          /* set (global) interrupt enable bit */
-}
-
-int main(void) {
-  // -------- Inits --------- //
-  LED_DDR = 0xff;                                   /* all LEDs active */
-  BUTTON_PORT |= (1 << BUTTON);                              /* pullup */
-  initInterrupt0();
-
-  // ------ Event loop ------ //
-  while (1) {
-
-    _delay_ms(200);
-    LED_PORT ^= (1 << LED0);
-
-  }                                                  /* End event loop */
-  return 0;                            /* This line is never reached */
+  EIMSK |= (1 << INT0);   //Enable INT0
+  EICRA |= (1 << ISC00);  //Trigger every button Change              /* trigger when button changes */
+  sei();                  //Enable global interrupts
 }
